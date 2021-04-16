7b13238b430f7f918c0bf069d19ad3698ac58faf
prestwia <prestwia@oregonstate.edu>
2021-04-15 17:36:22 -0700

    added whitespace to possible charTypes

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -7,16 +7,20 @@ def passGen(a):
     letters = string.ascii_letters
     digits = string.digits
     punctuation = string.punctuation
+    whitespace = " "
 
     password = ""
 
     for i in range(a):
-        charType = random.randint(1, 3)
+        charType = random.randint(1, 4)
         if charType == 1:
             password += random.choice(letters)
         elif charType == 2:
             password += random.choice(digits)
         elif charType == 3:
             password += random.choice(punctuation)
+        elif charType == 4:
+            password += whitespace
+    print(password)
 
-        
+passGen(10)       
