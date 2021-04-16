8832f551f7f2fb5ab5f72b33633ce2c7cf33ef07
prestwia <prestwia@oregonstate.edu>
2021-04-15 17:27:42 -0700

    added reference strings for character types

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -3,13 +3,19 @@ import random
 
 def passGen(a):
     random.seed()
-    charType = random.randint(1, 3)
-    
+
+    letters = string.ascii_letters
+    digits = string.digits
+    punctuation = string.punctuation
+
+
+    charType = random.randint(1, 4)
     if charType == 1:
-
+        print(random.choice(letters))
     elif charType == 2:
-
+        print(random.choice(digits))
     elif charType == 3:
+        print(random.choice(punctuation))
 
 
 passGen(1)
