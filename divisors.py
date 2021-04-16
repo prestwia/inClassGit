3144b1ecaed1196f188be14e7801a57c7441b3e6
prestwia <prestwia@oregonstate.edu>
2021-04-15 17:09:56 -0700

    implemented / checked logic for divisors function

--- divisors.py
+++ divisors.py
@@ -1,4 +1,11 @@
- def divisors(a):
-     divisors = []
+def divisors(a):
+    results = []
+    for i in range(1, a):
+        if a % i == 0:
+            print(i)
+
+
+divisors(10)
+
     
             
