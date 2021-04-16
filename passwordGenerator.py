02172f24e73715caaf861247d1d44776934bb5bf
prestwia <prestwia@oregonstate.edu>
2021-04-15 17:23:36 -0700

    added logic for selecting character type

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -2,3 +2,14 @@ import string
 import random 
 
 def passGen(a):
+    random.seed()
+    charType = random.randint(1, 3)
+    
+    if charType == 1:
+
+    elif charType == 2:
+
+    elif charType == 3:
+
+
+passGen(1)
